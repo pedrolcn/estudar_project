c2e14248ae4699d4512e401661003916ed6ad6ff
P.N <pedrolcn@gmail.com>
2018-02-13 13:14:08 -0200

    Feat: Added text input to quiz page

--- app_quiz/urls.py
+++ app_quiz/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = "app_quiz"
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('<int:pk>/', views.DetailView.as_view(), name='details'),
+    path('<int:pk>/', views.DetailView.as_view(), name='quiz'),
     path('<int:pk>/result/', views.ResultsView.as_view(), name='results'),
     path('<int:quiz_id>/submit/', views.submit, name='submit')
 ]
--- app_quiz/views.py
+++ app_quiz/views.py
@@ -4,7 +4,7 @@ from django.views import generic
 from django.utils import timezone
 from django.db.models import Count
 
-from .models import Quiz, Question, MultipleChoice
+from .models import Quiz, Question, MultipleChoice, TextAnswer
 
 
 class IndexView(generic.ListView):
@@ -14,12 +14,12 @@ class IndexView(generic.ListView):
     def get_queryset(self):
         """ Return the last 5 published questions"""
         return Quiz.objects.filter(
-            pub_date__lte=timezone.now()).order_by('-pub_date')
+            pub_date__lte=timezone.now()).order_by('pub_date')
 
 
 class DetailView(generic.DetailView):
     model = Quiz
-    template_name = 'app_quiz/details.html'
+    template_name = 'app_quiz/quiz.html'
 
 
 class ResultsView(generic.DetailView):
@@ -30,21 +30,39 @@ class ResultsView(generic.DetailView):
 def submit(request, quiz_id):
 
     right_answers = 0
+    answers = []
 
     quiz = Quiz.objects.annotate(num_questions=Count('question')).get(pk=quiz_id)
     q_num = quiz.num_questions
 
     for question in quiz.question_set.all():
-        if not question.textAnswer:
-            choice_id = request.POST['question%d' % question.id]
-            choice = MultipleChoice.objects.get(pk=choice_id)
-            if choice.isCorrectAnswer:
-                right_answers += 1
+        try:
+            answer = request.POST['question%d' % question.id]
+        except KeyError:
+            return render(request, 'app_quiz/quiz.html',
+                          {
+                              'quiz': Quiz.objects.get(pk=quiz_id),
+                              'error_message': "You didn't answer all questions."
+                          })
+        else:
+            if not question.textAnswer:
+                choice = MultipleChoice.objects.get(pk=answer)
+
+                if choice.isCorrectAnswer:
+                    right_answers += 1
+            else:
+                text_answer = question.textanswer_set.all()[0]
+
+                if answer == text_answer.correctAnswer:
+                    right_answers += 1
+
+            answers.append(answer)
 
     context_dict = {
         'quiz': quiz,
         'right_answers': right_answers,
-        'q_num': q_num
+        'q_num': q_num,
+        'answers': answers
     }
 
     return render(request, 'app_quiz/submit.html', context_dict)
