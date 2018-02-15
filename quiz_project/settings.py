0a4d47e09fd3efd187643f18dc077c719a124c56
P.N <pedrolcn@gmail.com>
2018-02-15 13:56:31 -0200

    Build: fixed settings

--- quiz_project/settings.py
+++ quiz_project/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'whitenoise.runserver_nostatic',
-    # 'django.contrib.staticfiles',
+    'django.contrib.staticfiles',
 ]
 
 MIDDLEWARE = [
